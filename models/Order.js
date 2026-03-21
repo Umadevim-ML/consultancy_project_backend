4f2c9e01b7ab96c84742fe8c2f1ceaaebb5da239
Umadevim-ML <umadevim.23aim@kongu.edu>
2026-03-21 13:09:58 +0530

    feat: add mobileNumber field to Order model and controller

--- models/Order.js
+++ models/Order.js
@@ -27,6 +27,7 @@ const orderSchema = new mongoose.Schema({
         postalCode: { type: String, required: true },
         country: { type: String, required: true },
     },
+    mobileNumber: { type: String, required: true },
     paymentMethod: { type: String, required: true },
     paymentResult: {
         id: { type: String },
